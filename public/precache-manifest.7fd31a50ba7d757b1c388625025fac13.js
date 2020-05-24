2b8264902723d629adedee48e6bf1d1b57cd3bb0
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-24 07:41:13 -0300

    build react app

---
+++ public/precache-manifest.7fd31a50ba7d757b1c388625025fac13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efec8fac91d65c3e0d10fbac92467233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4157188a7f381418b58",
+    "url": "/static/css/main.cde5f3bb.chunk.css"
+  },
+  {
+    "revision": "bf343c01aa600c82a7d8",
+    "url": "/static/js/2.f55b271c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f55b271c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4157188a7f381418b58",
+    "url": "/static/js/main.a3e7b782.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
